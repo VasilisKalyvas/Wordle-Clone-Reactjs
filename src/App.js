a17accfb4c0ba2c33ddfc5f9d44d96883cf2df67
Vasilis Kalyvas <101932947+VasilisKalyvas@users.noreply.github.com>
2025-03-04 21:05:29 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [solution, setSolution] = useState(null)
 
   useEffect(() => {
-    fetch('http://localhost:3001/solutions')
+    fetch('https://wordle-clone-reactjs.onrender.com/solutions')
     .then(res => res.json())
     .then(json => {
       const randomSolution = json[Math.floor(Math.random()*json.length)]
