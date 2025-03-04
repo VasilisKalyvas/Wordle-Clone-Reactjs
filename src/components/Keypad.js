1d6f75c6356d6f61d3ace7b008cac922fb682e6b
Vasilis Kalyvas <101932947+VasilisKalyvas@users.noreply.github.com>
2025-03-04 21:05:13 +0200

    Update Keypad.js

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -4,7 +4,7 @@ const Keypad = ({usedKeys}) => {
     const [letters, setLetters] = useState(null)
 
     useEffect(() => {
-      fetch('http://localhost:3001/letters')
+      fetch('https://wordle-clone-reactjs.onrender.com/letters')
         .then(res => res.json())
         .then(json => {
           setLetters(json)
